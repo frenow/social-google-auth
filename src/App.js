6114d0751e6b4bac1c94b51701ad04fd295468a8
Emerson D <frenow@outlook.com>
2022-08-03 14:39:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { auth } from '../src/services/firebase';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState([]);
+  function handleGoogleSignin(){
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then((result)=> {
+      setUser(result.user);
+      console.log(result.user);
+    })
+    .catch((error)=> {
+      console.log(error);
+    }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Utilizar autenticação social google.</h1>
+      <button onClick={handleGoogleSignin}>
+        Entrar com o google
+      </button>
+      <div>
+        <h3>{user.displayName}</h3>
+        <h4>{user.email}</h4>
+        {user.photoURL && (<img src={user.photoURL} alt="Foto do usuário" />)}
+      </div>
     </div>
   );
 }
